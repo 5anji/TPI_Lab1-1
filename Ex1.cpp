44459cfb1a21a15b4896730193ba74af3cd135b4
Gaby <gitlanm@gmail.com>
2020-03-26 13:14:54 +0200

    Fixed 1st task
    Used randomize

--- Ex1.cpp
+++ Ex1.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-  int i, j, k = 1, count = 100;
+  int i, j, k = 1, count = 100, numb1, numb2;
   float n = 0, perc;
 
+  srand((unsigned) time(0));
+  
   for (i = 1; i <= 4; i++) {
     for (j = 1; j <= count; j++) {
-      if (j%25 == 0) n++;
+      numb1 = (rand() % 6) + 1;
+      numb2 = (rand() % 6) + 1;
+      if (numb1 == 6 && numb2 == 6) n++;
     }
     
     perc = (n/count) * 100;
